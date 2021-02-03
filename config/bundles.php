ee53a5b622410252343eed5bb50b00f72f71bbbb
devBo <b.branez@agencesurf.com>
2021-02-03 15:23:00 +0100

    ADD : QR code generation on Produit persist

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
