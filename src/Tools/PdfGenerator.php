dc466f1336870c6adc724adb2154a013824967a3
devBo <b.branez@agencesurf.com>
2021-02-05 11:54:40 +0100

    UPDATE : pdf format for Dyno

--- src/Tools/PdfGenerator.php
+++ src/Tools/PdfGenerator.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\RouterInterface;
 
 class PdfGenerator
 {
-    const ITEMS_PER_PAGE = 12; // always an even number
     /**
      * @var string
      */
@@ -30,85 +29,32 @@ class PdfGenerator
         $this->router = $router;
     }
 
-    public function getPdf(array $products)
+    public function getPdfDyno(array $products)
     {
-        $files = $this->generatePdfFragments($products);
+        $pdf = new Fpdi('P', 'mm', [28, 89]);
+        $pdf->AddPage();
+        $pdf->setSourceFile($this->qrCodeDir . '/28x89_template.pdf');
+        $templateId = $pdf->importPage(1);
 
-        $pdf = new FPDI();
+        $pdf->useTemplate($templateId, 0, 0, 1);
+        $pdf->SetFont('Helvetica', '', 7);
+        $pdf->SetTextColor(255, 0, 0);
 
-        foreach ($files as $index => $file) {
-            $pageCount = $pdf->setSourceFile($file);
-            for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
-                $templateId = $pdf->importPage($pageNo);
-                $size = $pdf->getTemplateSize($templateId);
-
-                if ($size['width'] > $size['height']) {
-                    $pdf->AddPage('L', array($size['width'], $size['height']));
-                } else {
-                    $pdf->AddPage('P', array($size['width'], $size['height']));
-                }
-
-                $pdf->useTemplate($templateId);
-
-                $pdf->SetFont('Helvetica');
-                $pdf->SetXY(100, 0);
-                $pdf->Write(8, 'page ' . ($index + 1));
-            }
+        $currentProduct = $products[0];
+        $image = $this->qrCodeDir . "/images/{$currentProduct->getId()}.png";
+        if (!file_exists($image)) {
+            $this->generateQrCode($currentProduct);
         }
 
-        $pdf->Output();
-    }
+        $pdf->SetXY(7, 26);
+        $pdf->Write(0, 'id:' . $currentProduct->getId());
+        $pdf->SetXY(4, 4);
+        $pdf->Cell(20, 20, $pdf->Image($image, $pdf->GetX(), $pdf->GetY(), /*side*/ 20), 0, 0, 'C', false);
 
-    /**
-     * @param array $products
-     * @return array
-     */
-    private function generatePdfFragments(array $products): array
-    {
-        $totalItems = count($products);
-        $imagesPerPage = self::ITEMS_PER_PAGE;
-        $totalPages = ceil($totalItems / $imagesPerPage);
-        $files = [];
+        $tempFilePath = "{$this->qrCodeDir}/temp/dyno.pdf";
+        $pdf->Output($tempFilePath, 'F');
 
-        for ($page = 1; $page <= $totalPages; $page++) {
-            $pdf = new Fpdi();
-            $pdf->AddPage();
-            $pdf->setSourceFile($this->qrCodeDir . '/template.pdf');
-            $tplIdx = $pdf->importPage(1);
-
-            foreach (range(1, $imagesPerPage) as $item) {
-                $pdf->useTemplate($tplIdx, 10, 10, 100);
-                $pdf->SetFont('Helvetica');
-                $pdf->SetTextColor(255, 0, 0);
-
-                $currentItemIndex = $imagesPerPage * ($page - 1) + $item - 1;
-                if (!isset($products[$currentItemIndex])) {
-                    break;
-                }
-                $currentProduct = $products[$currentItemIndex];
-                $image = $this->qrCodeDir . "/images/{$currentProduct->getId()}.png";
-                if (!file_exists($image)) {
-                    $this->generateQrCode($currentProduct);
-                }
-
-                if ($item % 2 !== 0) {
-                    $y = 20 * $item + 20;
-                    $pdf->SetXY(40, $y);
-                    $pdf->Write(0, "Item ID : {$currentProduct->getId()}");
-                    $pdf->Cell(30, 30, $pdf->Image($image, $pdf->GetX() - 20, $pdf->GetY() - 33, /*side*/ 30), 0, 0, 'C', false);
-                } else {
-                    $y = 20 * ($item - 1) + 20;
-                    $pdf->SetXY(120, $y);
-                    $pdf->Write(0, "Item ID : {$currentProduct->getId()}");
-                    $pdf->Cell(30, 30, $pdf->Image($image, $pdf->GetX() - 20, $pdf->GetY() - 33, /*side*/ 30), 0, 0, 'C', false);
-                }
-            }
-            $tempFile = "{$this->qrCodeDir}/temp/page{$page}.pdf";
-            $pdf->Output($tempFile, 'F');
-            $files[] = $tempFile;
-        }
-
-        return $files;
+        return $tempFilePath;
     }
 
     /**
